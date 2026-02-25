b8453edcc0ce4fb69c9119f35867845760b7dd68
Skankydev <simon.schenck@gmail.com>
2026-02-25 17:41:59 +0100

    soon

--- src_front/view/part/paginator.php
+++ src_front/view/part/paginator.php
@@ -1,11 +1,11 @@
 <?php $aClass = 'btn-mini'; ?>
 <nav id="Paginator" class="text-center p-m">
 	
-	<?php $class = $aClass; if($first==$prev) $class .= ' btn-disabled'; ?>	
+	<?php $class = $aClass; if($first==$prev) $class .= ' btn-disabled'; ?>
 	
 		<?php 
 			$get['page'] = $first;
-			echo $this->link('&#60;&#60;',[...$link,'get'=>$get],['class'=>$class]); 
+			echo $this->link('&#60;&#60;',[...$link,'get'=>$get],['class'=>$class]);
 		?>
 	
 	<?php $class = $aClass; if($page==$prev) $class .= ' btn-disabled'; ?>
@@ -15,14 +15,14 @@
 		echo $this->link('&#60;',[...$link,'get'=>$get],['class'=>$class]); ?>
 	
 	<?php for ($i=$start; $i < $stop; ++$i): ?> 
-		<?php $class = $aClass; if($i==$page) $class .= ' btn-success'; ?>	
+		<?php $class = $aClass; if($i==$page) $class .= ' btn-success'; ?>
 		
 		<?php 
 		$get['page'] = $i;
 		echo $this->link($i,[...$link,'get'=>$get],['class'=>$class]); ?>
 		
 	<?php endfor ?>
-	<?php $class = $aClass; if($page==$next) $class .= ' btn-disabled'; ?>	
+	<?php $class = $aClass; if($page==$next) $class .= ' btn-disabled'; ?>
 	
 	<?php 
 	$get['page'] = $next;
